61e0b7271caa7ac8f4847ebc6aaee640e72e3035
dsapr <785722840@qq.com>
2022-04-08 18:02:45 +0800

    添加导航栏，完善目录结构

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,39 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    base: '/blog-vuepress/',
+    lang: `zh-CN`,
+    title: 'dsapr',
+    description: '是 dsapr 的博客呢 ☺',
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        logo: '/images/logo.png',
+        repo: 'git@github.com:dsapr/blog-vuepress.git',
+        repoLabel: '博客仓库',
+        contributors: true,
+        navbar: [
+            {
+                text: '概览',
+                link: '/overview/'
+            },
+            {
+                text: 'Java',
+                link: '/java/'
+            },
+            {
+                text: '站点',
+                children: [
+                    {
+                        text: 'Github',
+                        link: 'https://github.com/dsapr'
+                    },
+                    {
+                        text: 'Gitee',
+                        link: 'https://gitee.com/dsapr'
+                    },
+                ]
+            }
+        ]
+    }
+})
